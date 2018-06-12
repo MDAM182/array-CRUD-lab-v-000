d254ae0cd22846d2e1e6105903f923b609ae054a
Michael Morris <mdam2345@gmail.com>
2018-06-12 02:44:31 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,6 +1,5 @@
 def create_an_empty_array
   array = []
-
 end
 
 def create_an_array
@@ -16,7 +15,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-
+  array.pop("arrays!")
 end
 
 def remove_element_from_start_of_array(array)
